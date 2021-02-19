f462c685dc0b860fd34f233ef9ec0e1eda85b28f
analubarreto <analuibm@gmail.com>
2021-02-19 15:17:14 -0300

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	// Imports - Pages
+	import Home from './router/Home.svelte';
+	import About from './router/About.svelte';
+	import Checkout from './router/Checkout.svelte';
+	import Login from './router/Login.svelte';
+	import Products from './router/Products.svelte';
+	import ProductTemplate from './router/ProductTemplate.svelte';
+
+	// Imports - Router
+	import { Router, Route, Link } from 'svelte-routing';
+</script>
+
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/about">About</Link>
+		<Link to="/products">Products</Link>
+	</nav>
+	<Route path="/" component={Home} />
+	<Route path="/about" component={About} />
+	<Route path="/login" component={Login} />
+	<Route path="/products" component={Products} />
+	<Route path="/products/:id" component={ProductTemplate} />
+</Router>
---
+++ src/router/ProductTemplate.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let id;
+</script>
+
+<h1>Welcome to product template page with id: {id}</h1>
